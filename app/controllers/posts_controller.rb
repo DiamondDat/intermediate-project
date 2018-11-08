51aedb9e43b8c040012b13a4a31a65be932a18d1
DiamondDat <diamond.dat@1pac.vn>
2018-11-08 12:04:32 +0700

    Make search bar and category filters in branch pages functional - Add by_category, by_branch and search scopes in the Post model - Modify the get_posts method in PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,4 +17,20 @@ class PostsController < ApplicationController
     @categories = Category.where(branch: params[:action])
     @posts = Post.limit(30).paginate(page: params[:page])
   end
+
+  def get_posts
+    branch = params[:action]
+    search = params[:search]
+    category = params[:category]
+
+    if category.blank? && search.blank?
+      posts = Post.by_branch(branch).all
+    elsif category.blank? && search.present?
+      posts = Post.by_branch(branch).search(search)
+    elsif category.present? && search.blank?
+      posts = Post.by_category(branch, category)
+    elsif category.present? && search.present?
+      posts = Post.by_category(branch, category).search(search)
+    end
+  end
 end
