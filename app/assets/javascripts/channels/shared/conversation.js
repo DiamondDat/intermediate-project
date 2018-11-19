47bce22f21ecb11508dce6a140143c803e47599d
DiamondDat <diamond.dat@1pac.vn>
2018-11-19 15:31:25 +0700

    Broadcast a new message - Inside the Private::Message define an after_create_comit callback method. - Create a Private::MessageBroadcastJob - Define a received function inside the   assets/javascripts/channels/private/conversation.js - Create a conversation.js inside the   assets/javascripts/channels/shared

---
+++ app/assets/javascripts/channels/shared/conversation.js
@@ -0,0 +1,29 @@
+// finds a conversation in the DOM
+function findConv(conversation_id, type) {
+  // if a current conversation is opened in the messenger
+  var messenger_conversation = $('body .conversation');
+  if(messenger_conversation.length) {
+    // conversation is opened in the messenger
+    return messenger_conversation;
+  } else {
+    // conversation is opened in a popup window
+    var data_attr = "[data-" + type + "conversation-id='" + conversation_id + "']";
+    var conversation = $('body').find(data_attr);
+    return conversation;
+  }
+}
+
+checks if a conversation window is rendered and visible on a browser
+function ConvRendered(conversation_id, type) {
+  // if a current conversation is opened in the messenger
+  if($('body .conversation').length) {
+    // conversation is opened in the messenger
+    // so it automatically means that is visible
+    return true;
+  } else {
+    // conversations is opened in a popup window
+    // check if the window is collapsed or expanded
+    var visibility = conversation.find('.panel-body').is(':visible');
+    return visibility;
+  }
+}
